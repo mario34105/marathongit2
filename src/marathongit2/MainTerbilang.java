da90b2d004103e798142092d249c810c24cd0b4e
Mario yaputra <mario34105@gmail.com>
2016-08-20 14:41:26 +0700

    add feature terbilang

---
+++ src/marathongit2/MainTerbilang.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+/**
+ *
+ * @author My pc
+ */
+public class MainTerbilang {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println(Terbilang.proses(27400));
+        // TODO code application logic here
+    }
+    
+}
